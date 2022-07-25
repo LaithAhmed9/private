52535444f47747d87f7bcf2e4c6feaf4df5cc239
LaithAhmed9 <github.eng.laith@gmail.com>
2022-07-25 12:53:30 +0300

    uploadnew

--- src/components/Home.js
+++ src/components/Home.js
@@ -8,11 +8,11 @@ const Home = () => {
   return (
     <div 
         name='home'
-        className='h-screen w-full bg-gradient-to-b from-black via-black to-gray-800'
+        className=' h-[800px] w-full bg-gradient-to-b from-black via-black to-gray-800'
         >
         <div className="max-w-screen-lg mx-auto flex flex-col items-center justify-center h-full px-4 md:flex-row">
         <div className="flex flex-col justify-center h-full text-center">
-            <h2 className="text-4xl sm:text-7xl font-bold text-white">
+            <h2 className=" text-4xl sm:text-7xl font-bold text-white">
                 I'm a Full Stack Developer
             </h2>
                 <p className="text-gray-500 py-4 max-w-md">
@@ -36,7 +36,7 @@ const Home = () => {
           <img
             src={HeroImage}
             alt="my profile"
-            className="rounded-2xl mx-auto w-11/12 md:w-full"
+            className=" pb-14 rounded-2xl mx-auto w-2/3 md:w-full"
           />
         </div>
         </div>
